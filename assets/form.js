1766188bd2bd7b4602c006608170e8e49ec6d0a2
Hugo Hsi <hhsi@hugodehsign.com>
2024-03-16 23:43:07 -0400

    reorganized some functions between files

--- assets/form.js
+++ assets/form.js
@@ -1,50 +1,50 @@
 // get references needed for form logic
-const formContainer = $("#form-container");
-const resultsHeading = $("#results-heading");
+const formContainer = $('#form-container');
+const resultsHeading = $('#results-heading');
 
 function generateDropdownList() {
   // init array of dropdown options
   // loop through later to create dropdown
   const dropdownOptions = [
     {
-      value: "audio",
-      display: "Audio Recordings",
+      value: 'audio',
+      display: 'Audio Recordings',
     },
     {
-      value: "books",
-      display: "Books/PrintedMaterial",
+      value: 'books',
+      display: 'Books/PrintedMaterial',
     },
     {
-      value: "film-and-videos",
-      display: "Film, Videos",
+      value: 'film-and-videos',
+      display: 'Film, Videos',
     },
     {
-      value: "legislation",
-      display: "Legislation",
+      value: 'legislation',
+      display: 'Legislation',
     },
     {
-      value: "manuscripts",
-      display: "Manuscripts/Mixed Material",
+      value: 'manuscripts',
+      display: 'Manuscripts/Mixed Material',
     },
     {
-      value: "maps",
-      display: "Maps",
+      value: 'maps',
+      display: 'Maps',
     },
     {
-      value: "newspapers",
-      display: "Newspapers",
+      value: 'newspapers',
+      display: 'Newspapers',
     },
     {
-      value: "photos",
-      display: "Photos, Print, Drawings",
+      value: 'photos',
+      display: 'Photos, Print, Drawings',
     },
     {
-      value: "notated-music",
-      display: "Printed Music (such as sheet music)",
+      value: 'notated-music',
+      display: 'Printed Music (such as sheet music)',
     },
     {
-      value: "web-archive",
-      display: "Web Archives",
+      value: 'web-archive',
+      display: 'Web Archives',
     },
   ];
 
@@ -68,7 +68,7 @@ function generateDropdownList() {
 function createForm() {
   // create all elements
   const form = $('<form class="d-flex flex-column gap-2">');
-  const headingEl = $("<h1>");
+  const headingEl = $('<h1>');
   const inputEl = $(
     '<input type="text" class="form-control" placeholder="Search!">'
   );
@@ -76,9 +76,9 @@ function createForm() {
   const submitEl = $('<button type="submit" class="btn btn-success">');
 
   // build elements
-  headingEl.text("Library of Congress Search Engine");
+  headingEl.text('Library of Congress Search Engine');
   dropdownEl.html(generateDropdownList()); // we call the generateDropdownList function here. Since it returned an array of <option> elements, we can apply it to the html directly here
-  submitEl.text("Click Me");
+  submitEl.text('Click Me');
 
   //place all elements
   form.append(headingEl).append(inputEl).append(dropdownEl).append(submitEl);
@@ -88,81 +88,3 @@ function createForm() {
   // see object destructuring for this syntax: https://www.w3schools.com/react/react_es6_destructuring.asp
   return { form, inputEl, dropdownEl, submitEl };
 }
-
-async function fetchData({ format, search }) {
-  // create url with form values
-  const url = `https://www.loc.gov/${format}/?q=${search}&fo=json`;
-  try {
-    // fetch data
-    const res = await fetch(url);
-    const data = await res.json();
-    console.log(data);
-    // loop through array and create new array of necessary data
-    const articlesArray = data.content.results.map((article) => {
-      // return object to map with necessary data
-      return {
-        title: article.title,
-        date: article.date,
-        subject: article.subject,
-        // description might be undefined, if undefined return 'no description...'
-        description: article.description
-          ? article.description
-          : ["No description for this entry."],
-        button: article.id,
-      };
-    });
-
-    container.empty(); // Clear the container before appending new cards
-    return articlesArray;
-    // need title, date, subject, description
-  } catch (error) {
-    resultsHeading.text("Error");
-    console.error(error);
-  }
-}
-
-function titleCaseString(str) {
-  const words = str.split(' ');
-  const capWords = words
-    .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
-    .join(' ');
-  return capWords;
-}
-
-function handleSubmit(e, { inputEl, dropdownEl }) {
-  // prevent auto refresh on form submit
-  e.preventDefault();
-
-  // we can get access to the elements here, as well as their value like so
-  // console.log(inputEl, inputEl.val());
-  // console.log(dropdownEl, dropdownEl.val());
-
-  location.assign(
-    `./results.html?format=${dropdownEl.val()}&search?query=${inputEl.val()}`
-  );
-
-  // fetchData({ inputEl, dropdownEl });
-}
-
-function initFetch() {
-  const currentUrl = location.search;
-  const searchParams = new URLSearchParams(currentUrl);
-  const format = searchParams.get("format");
-  const search = searchParams.get("search?query");
-  return { format, search };
-}
-
-// this function runs when the document is ready for dom manipulation
-$(document).ready(function () {
-  // eg. formEls = { form: form, inputEl: inputEl, dropdownEl: drop...}
-  const formEls = createForm();
-
-  // on runs handleSubmit function when form is submitted
-  formEls.form.on("submit", (e) => handleSubmit(e, formEls));
-  const params = initFetch();
-  const data = fetchData(params);
-
-  resultsHeading.text(titleCaseString(params.search));
-
-  createCards(data);
-});
--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,44 @@
 const container = $('#results-container'); // Use jQuery to select the container
-const spacer = $('#content-spacer');
+
+function initFetch() {
+  const currentUrl = location.search;
+  const searchParams = new URLSearchParams(currentUrl);
+  const format = searchParams.get('format');
+  const search = searchParams.get('search?query');
+  return { format, search };
+}
+
+async function fetchData({ format, search }) {
+  // create url with form values
+  const url = `https://www.loc.gov/${format}/?q=${search}&fo=json`;
+  try {
+    // fetch data
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+    // loop through array and create new array of necessary data
+    const articlesArray = data.content.results.map((article) => {
+      // return object to map with necessary data
+      return {
+        title: article.title,
+        date: article.date,
+        subject: article.subject,
+        // description might be undefined, if undefined return 'no description...'
+        description: article.description
+          ? article.description
+          : ['No description for this entry.'],
+        button: article.id,
+      };
+    });
+
+    container.empty(); // Clear the container before appending new cards
+    return articlesArray;
+    // need title, date, subject, description
+  } catch (error) {
+    resultsHeading.text('Error');
+    console.error(error);
+  }
+}
 
 // Function to create a card element
 function createCardElement({ title, date, subject, description, button }) {
@@ -39,9 +78,49 @@ async function createCards(data) {
     articlesData.forEach((article) => {
       const card = createCardElement(article);
       container.append(card); // Use jQuery to append the card element to the container
-      spacer.remove();
     });
   } catch (err) {
     console.error(err);
   }
 }
+
+function titleCaseString(str) {
+  const words = str.split(' ');
+  const capWords = words
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+    .join(' ');
+  return capWords;
+}
+
+function handleSubmit(e, { inputEl, dropdownEl }) {
+  // prevent auto refresh on form submit
+  e.preventDefault();
+
+  location.assign(
+    `./results.html?format=${dropdownEl.val()}&search?query=${inputEl.val()}`
+  );
+}
+
+// this function runs when the document is ready for dom manipulation
+$(document).ready(function () {
+  // eg. formEls = { form: form, inputEl: inputEl, dropdownEl: drop...}
+  const formEls = createForm();
+
+  // on runs handleSubmit function when form is submitted
+  formEls.form.on('submit', (e) => handleSubmit(e, formEls));
+  const params = initFetch();
+
+  // prevent fetching and creation of cards if no url parameters, notably on the home screen
+  if (!params.format || !params.search) {
+    return;
+  }
+
+  // get data from fetch request
+  const data = fetchData(params);
+
+  // change the heading to the search parameter in the url
+  resultsHeading.text(titleCaseString(params.search));
+
+  // create cards function
+  createCards(data);
+});
